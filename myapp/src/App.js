235766aa3581b5e16d017ffb62f4fabd3d9f526f
Ezzeddine Jlassi <jlassiezzeddine@gmail.com>
2021-05-07 22:47:56 +0100

    Home Page first  commit

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -6,23 +6,24 @@ import Navigation from "./Components/Navigation/Navigation";
 import Account from "./pages/Account/Account";
 import Reservations from "./pages/Reservations/Reservations";
 import Favorites from "./pages/Favorites/Favorites";
-import {PaperFormLogin,PaperFormRegister} from "./pages/Login/Paper"
+import { PaperFormLogin, PaperFormRegister } from "./pages/Login/Paper";
 import { ThemeProvider } from "@material-ui/core";
 import { theme } from "./theme";
 function App() {
-  const [auth, setAuth] = React.useState(false);
+  //Use isAuth for the global authentication state (or Redux)
+  const [isAuth, setIsAuth] = React.useState(false);
+  //******************************************************** */
+
   const [location, setLocation] = React.useState("Tunis");
   return (
     <>
-  <ThemeProvider theme={theme}>
-  <Navigation
-        auth={auth}
-        setAuth={setAuth}
-        setLocation={setLocation}
-        location={location}
-      />
-  </ThemeProvider>
-   
+      <ThemeProvider theme={theme}>
+        <Navigation
+          isAuth={isAuth}
+          setLocation={setLocation}
+          location={location}
+        />
+      </ThemeProvider>
 
       <Switch>
         <Route exact path="/" component={homePage} />
